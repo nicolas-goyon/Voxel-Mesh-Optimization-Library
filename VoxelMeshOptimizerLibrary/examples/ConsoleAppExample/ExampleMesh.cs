d511abd258587222717c24f32274a59bfde8823f
nicolas-goyon <38950115+nicolas-goyon@users.noreply.github.com>
2025-08-11 09:23:38 +0000

    Object exporter first iteration OK

--- VoxelMeshOptimizerLibrary/examples/ConsoleAppExample/ExampleMesh.cs
+++ VoxelMeshOptimizerLibrary/examples/ConsoleAppExample/ExampleMesh.cs
@@ -4,6 +4,11 @@ namespace ConsoleAppExample;
 
 public class ExampleMesh : Mesh
 {
-    public IReadOnlyList<(float x, float y, float z)> Vertices { get; init; } = new List<(float, float, float)>();
-    public IReadOnlyList<int> Triangles { get; init; } = new List<int>();
+    public List<MeshQuad> Quads { get; set; }
+
+    public ExampleMesh(List<MeshQuad> quads)
+    {
+        Quads = quads;
+    }
+
 }
--- VoxelMeshOptimizerLibrary/examples/ConsoleAppExample/Program.cs
+++ VoxelMeshOptimizerLibrary/examples/ConsoleAppExample/Program.cs
@@ -2,21 +2,39 @@ using VoxelMeshOptimizer.Core;
 using VoxelMeshOptimizer.Core.OptimizationAlgorithms.DisjointSet;
 
 namespace ConsoleAppExample;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        ushort[,,] voxels = {
-            { {0,0,0} , {0,0,0}, {0,0,0}},
-            { {0,0,0} , {0,0,0}, {0,0,0}},
-            { {0,0,0} , {0,0,0}, {0,0,0}},
-        };
-        var exampleChunk = new ExampleChunk(voxels);
-        var optimizer = new DisjointSetMeshOptimizer();
-        Mesh optimizedMesh = optimizer.Optimize(exampleChunk);
+        // ushort[,,] voxels = {
+        //     { {0,0,0} , {0,0,0}, {0,0,0}},
+        //     { {0,0,0} , {0,0,0}, {0,0,0}},
+        //     { {0,0,0} , {0,0,0}, {0,0,0}},
+        // };
+        // var exampleChunk = new ExampleChunk(voxels);
+        // var optimizer = new DisjointSetMeshOptimizer();
+        // Mesh optimizedMesh = optimizer.Optimize(exampleChunk);
 
-        Console.WriteLine("Mesh optimized successfully!");
+        // Console.WriteLine("Mesh optimized successfully!");
+
+        var quad = new MeshQuad
+        {
+            Vertex0 = new Vector3(0, 0, 0),
+            Vertex1 = new Vector3(1, 0, 0),
+            Vertex2 = new Vector3(1, 1, 0),
+            Vertex3 = new Vector3(0, 1, 0),
+            Normal = new Vector3(0, 0, 1),
+            VoxelID = 1
+        };
+        var list = new List<MeshQuad>();
+        list.Add(quad);
+        var mesh = new ExampleMesh(list);
+        var path = Path.Combine("/workspaces/Voxel-Mesh-Optimization-Library/VoxelMeshOptimizerLibrary/examples", Path.GetRandomFileName() + ".obj");
+
+        // Act
+        ObjExporter.Export(mesh, path);
     }
 }
 
